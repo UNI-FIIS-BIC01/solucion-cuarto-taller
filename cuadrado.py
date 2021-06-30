406d3fabbdba9df7fc7308d4a9fdec044bc8169a
Carlos G. Gavidia <cgavidiac@gmail.com>
2021-06-30 20:10:41 +0100

    Revert "Solución al taller"
    This reverts commit bf3fa0d644472acd9d8b6719cd8bf841b2817e60.

--- cuadrado.py
+++ cuadrado.py
@@ -4,22 +4,7 @@ from math import sqrt
 class Cuadrado(object):
 
     def __init__(self, longitud_lado, unidad_medida):
-        self.longitud_lado = longitud_lado
-        self.unidad_medida = unidad_medida
-
-    def calcular_perimetro(self):
-        return 4 * self.longitud_lado
-
-    def calcular_area(self):
-        return self.longitud_lado ** 2
-
-    def calcular_diagonal(self):
-        return sqrt(2 * self.longitud_lado ** 2)
-
-    def __str__(self):
-        return f"Lado: {self.longitud_lado} {self.unidad_medida}\nPerimetro: {self.calcular_perimetro()} {self.unidad_medida}\n" \
-               f"Area: {self.calcular_area()} {self.unidad_medida}**2\nDiagonal: {round(self.calcular_diagonal(), 2)} " \
-               f"{self.unidad_medida}"
+        pass
 
 
 if __name__ == "__main__":
--- factura.py
+++ factura.py
@@ -1,36 +1,10 @@
 class Factura(object):
 
     def __init__(self, codigo_producto, descripcion_producto, cantidad, precio_unitario):
-
-        if cantidad < 0:
-            raise ValueError("La cantidad debe ser un entero positivo")
-
-        if precio_unitario < 0:
-            raise ValueError("El precio unitario debe ser un decimal positivo")
-
-        self.codigo_producto = codigo_producto
-        self.descripcion_producto = descripcion_producto
-        self.cantidad = cantidad
-        self.precio_unitario = precio_unitario
-
-    def calcular_total_factura(self):
-        return self.precio_unitario * self.cantidad
-
-    def __str__(self):
-        decimales = 2
-        return f"Codigo: {self.codigo_producto}\nDescripcion: {self.descripcion_producto}\nCantidad: {self.cantidad}\n" \
-               f"Precio Unitario: {round(self.precio_unitario, decimales)}\n" \
-               f"Total Factura: {round(self.calcular_total_factura(), decimales)}"
+        pass
 
 
 if __name__ == "__main__":
     una_factura = Factura("1", "Laptop", 10, 3000.987)
-    print(una_factura.codigo_producto)  # En consola: 1
-    print(una_factura.descripcion_producto)  # En consola: Laptop
-    print(una_factura.cantidad)  # En consola: 10
-    print(una_factura.precio_unitario)  # En consola: 3000.987
-    print(una_factura.calcular_total_factura())  # En consola: 30009.870000000003
+    print(una_factura)
 
-    # una_factura = Factura("1", "Laptop", -2, 3000.987)
-
-    # una_factura = Factura("1", "Laptop", 2, -3000.987)
