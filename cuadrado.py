e7cd65afa27486865a0ca6150d7b1143c2644cbf
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2021-07-01 09:30:13 +0100

    Revert "Revert "Solución al taller""
    This reverts commit 406d3fabbdba9df7fc7308d4a9fdec044bc8169a.

--- cuadrado.py
+++ cuadrado.py
@@ -4,7 +4,22 @@ from math import sqrt
 class Cuadrado(object):
 
     def __init__(self, longitud_lado, unidad_medida):
-        pass
+        self.longitud_lado = longitud_lado
+        self.unidad_medida = unidad_medida
+
+    def calcular_perimetro(self):
+        return 4 * self.longitud_lado
+
+    def calcular_area(self):
+        return self.longitud_lado ** 2
+
+    def calcular_diagonal(self):
+        return sqrt(2 * self.longitud_lado ** 2)
+
+    def __str__(self):
+        return f"Lado: {self.longitud_lado} {self.unidad_medida}\nPerimetro: {self.calcular_perimetro()} {self.unidad_medida}\n" \
+               f"Area: {self.calcular_area()} {self.unidad_medida}**2\nDiagonal: {round(self.calcular_diagonal(), 2)} " \
+               f"{self.unidad_medida}"
 
 
 if __name__ == "__main__":
